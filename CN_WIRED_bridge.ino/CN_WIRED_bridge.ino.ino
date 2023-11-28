91a7b5507c6212f674ea05d0f3c7264856e9aa81
Pavel Fedin <pavel_fedin@mail.ru>
2023-11-28 22:10:53 +0300

    CN_WIRED_bridge: Support for two receivers
    For the time being i fail at sending a proper message, so let's support dumping
    both sides of the communication. Rewrite the RX code as a class, instantiate two
    objects on different pins

--- CN_WIRED_bridge.ino/CN_WIRED_bridge.ino.ino
+++ CN_WIRED_bridge.ino/CN_WIRED_bridge.ino.ino
@@ -1,57 +1,78 @@
-#define RX_PIN 3 // PD2
-#define TX_PIN 2 // PD3
-
-static int rx_state;
-static unsigned long start;
+// Pins we're using. RX2 is optional, can be commented out if not used
+#define RX1_PIN 2 // PD3
+#define RX2_PIN 3 // PD2
+#define TX_PIN  4 // PD4
 
 #define PKT_LEN 8
-#define RX_LEN PKT_LEN + 1
+// Our RX buffers are prefixed by one byte, where start bit is stored.
+// See start()
+#define RX_LEN  PKT_LEN + 1
 
-static uint8_t rx_buffer[RX_LEN + 1];
-static int rx_bytes;
-static int rx_bits;
+class Receiver {
+  public:
+    Receiver(int pin) : pin(pin) {}
 
-static uint8_t tx_buffer[PKT_LEN + 1];
-static int tx_bytes;
-static int tx_bits;
+    void begin(void(*handler)()) {
+      state = 1;
+      pinMode(pin, INPUT);
+      attachInterrupt(digitalPinToInterrupt(pin), handler, CHANGE);
+    }
 
-static void receiveBit(uint8_t v) {
-  rx_buffer[rx_bytes] |= (v << rx_bits);
-  if (rx_bits == 7) {
-    rx_bits = 0;
-    rx_bytes++;
-    rx_buffer[rx_bytes] = 0;
-  } else {
-    rx_bits++;
-  }
-}
+    void reset() {
+      rx_bytes = -1; // This signals "Wait for sync" state
+    }
 
-static void resetRx() {
-  rx_bytes = -1;
-}
+    bool isReceiving() const {
+      return rx_bytes >= 0 && rx_bytes < RX_LEN;
+    }
 
-static void startRx() {
-  // The packet always starts with a single bit of 1, which is apparently
-  // a start bit, so the 0th byte will contain this single bit. This also lets
-  // us make sure that we're correct, and it's indeed always 1.
-  // The next bit is going to be LSB of 1st byte.
-  rx_bytes     = 0;
-  rx_bits      = 7;
-  rx_buffer[0] = 0;
-}
+    bool isDataReady() const {
+      return rx_bytes == RX_LEN;
+    }
 
-static bool isReceiving() {
-  return rx_bytes >= 0 && rx_bytes < RX_LEN;
-}
+    const uint8_t* getBuffer() const {
+      return buffer;
+    }
 
-static bool isDataReady() {
-  return rx_bytes == RX_LEN;
-}
+    void onInterrupt();
 
-void rxPinInt() {
-  int new_state = digitalRead(RX_PIN);
+  private:
+    void start() {
+      // The packet always starts with a single bit of 1, which is apparently
+      // a start bit, so the 0th byte will contain this single bit. This also lets
+      // us make sure that we're correct, and it's indeed always 1.
+      // The next bit is going to be LSB of 1st byte.
+      rx_bytes  = 0;
+      rx_bits   = 7;
+      buffer[0] = 0;
+    }
 
-  if (new_state == rx_state) {
+    void receiveBit(uint8_t v) {
+      buffer[rx_bytes] |= (v << rx_bits);
+
+      if (rx_bits == 7) {
+        rx_bits = 0;
+        rx_bytes++;
+        // Prepare the next byte. We don't check for overflow for speed, that's
+        // why we need one spare trailing byte
+        buffer[rx_bytes] = 0;
+      } else {
+        rx_bits++;
+      }
+    }
+
+    uint8_t       buffer[RX_LEN + 1];
+    unsigned long pulse_start; // Pulse start time
+    unsigned int  state;       // Current line state
+    int           rx_bytes;    // Bytes counter
+    int           rx_bits;     // Bits counter
+    const int     pin;
+};
+
+void Receiver::onInterrupt() {
+  int new_state = digitalRead(pin);
+
+  if (new_state == state) {
     return; // Some rubbish
   }
 
@@ -59,39 +80,24 @@ void rxPinInt() {
 
   if (new_state) {
     // LOW->HIGH, start of data bit
-    if (now - start > 2000) {
-      startRx(); // Got SYNC pulse, start receiving data
+    if (now - pulse_start > 2000) {
+      start(); // Got SYNC pulse, start receiving data
     }
   } else {
     // HIGH->LOW, start of SYNC or end of data bit
     if (isReceiving()) {
       // High bit - 1000us, low bit - 400us
-      uint8_t bit = (now - start) > 700;
+      uint8_t bit = (now - pulse_start) > 700;
       receiveBit(bit);
     }
   }
 
-  start = now;
-  rx_state = new_state;
+  // A new state has begun at 'now' microseconds
+  pulse_start = now;
+  state       = new_state;
 }
 
-static void resetTx() {
-  tx_bytes = 0;
-  tx_bits  = 4;
-  memset(tx_buffer, 0, PKT_LEN);
-}
-
-static void queueDigit(uint8_t v) {
-  if (tx_bytes == PKT_LEN) {
-    return;
-  }
-  tx_buffer[tx_bytes] |= (v << tx_bits);
-  tx_bits ^= 4;
-  if (tx_bits) {
-    tx_bytes++;
-  }
-}
-
+// A very simple self-explanatory data send routine
 static void send(const uint8_t* data, int len) {
   digitalWrite(TX_PIN, 0);
   delayMicroseconds(2500); // SYNC low
@@ -113,17 +119,53 @@ static void send(const uint8_t* data, int len) {
   digitalWrite(TX_PIN, 1); // Idle high
 }
 
+static Receiver rx1(RX1_PIN);
+static Receiver rx2(RX2_PIN);
+
+// TX buffer, where we read data from the serial port
+static uint8_t tx_buffer[PKT_LEN + 1];
+static int tx_bytes;
+static int tx_bits;
+
+static void resetTx() {
+  tx_bytes = 0;
+  tx_bits  = 4;
+  memset(tx_buffer, 0, PKT_LEN);
+}
+
+static void queueDigit(uint8_t v) {
+  if (tx_bytes == PKT_LEN) {
+    return;
+  }
+  tx_buffer[tx_bytes] |= (v << tx_bits);
+  tx_bits ^= 4;
+  if (tx_bits) {
+    tx_bytes++;
+  }
+}
+
+// Our interrupt handlers don't get any arguments, so we have to provide these bridges
+static void rx1PinInt() {
+  rx1.onInterrupt();
+}
+
+#ifdef RX2_PIN
+static void rx2PinInt() {
+  rx2.onInterrupt();
+}
+#endif
+
 void setup() {
   Serial.begin(115200);
 
-  rx_state = 1;
-  resetRx();
-  resetTx();
+  rx1.begin(rx1PinInt);
+#ifdef RX2_PIN
+  rx2.begin(rx2PinInt);
+#endif
 
-  pinMode(RX_PIN, INPUT);
+  resetTx();
   pinMode(TX_PIN, OUTPUT);
   digitalWrite(TX_PIN, 1);
-  attachInterrupt(digitalPinToInterrupt(RX_PIN), rxPinInt, CHANGE);
 }
 
 static void dump(const uint8_t* buffer, int len) {
@@ -137,11 +179,18 @@ static void dump(const uint8_t* buffer, int len) {
 }
 
 void loop() {
-  if (isDataReady()) {
-    Serial.print("Rx ");
-    dump(rx_buffer, RX_LEN);
-    resetRx();
+  if (rx1.isDataReady()) {
+    Serial.print("Rx1 ");
+    dump(rx1.getBuffer(), RX_LEN);
+    rx1.reset();
   }
+#ifdef RX2_PIN
+  if (rx2.isDataReady()) {
+    Serial.print("Rx2 ");
+    dump(rx2.getBuffer(), RX_LEN);
+    rx2.reset();
+  }
+#endif
   if (Serial.available() > 0) {
     int c = Serial.read();
     if (c == '\n' || c == '\r') {
