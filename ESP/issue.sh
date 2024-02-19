530b854d918de70b7a98e49d6c1450fccc0ed263
Pavel Fedin <pavel_fedin@mail.ru>
2024-02-19 22:18:10 +0300

    ESP: Support betarelease
    Borrowed from Faikin-ESP32

--- ESP/issue.sh
+++ ESP/issue.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+ISSUE_KIND=$1
+
+if [ -z "$ISSUE_KIND" ]; then
+	echo Required parameter is missing
+	exit 255
+fi
+
 CHECK=$(shell git diff-index --name-status HEAD)
 if [ -n "$CHECK" ]; then
 	echo There are uncommitted changes in the repository:
@@ -8,6 +15,6 @@ if [ -n "$CHECK" ]; then
 fi
 
 make
-cp Faikin-*.bin release
-git commit -a -m release
+cp Faikin-*.bin $ISSUE_KIND
+git commit -a -m $ISSUE_KIND
 git push
